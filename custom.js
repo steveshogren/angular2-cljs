d8c12d41ac2168d289ef7b56fa65123b2134e92f
Jack <steve.a.shogren@gmail.com>
2016-05-02 07:01:57 -0400

    Lame double click with state

--- custom.js
+++ custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    $("div").click(function() {alert("Hello, world!");});
+    var times = 0;
+    $("p").click(function() {
+        times++;
+        if (times===1) {
+            $("body").append("<p>single click</p>");
+        }
+        if (times===2) {
+            $("body").append("<p>double click</p>");
+        }
+    });
 });
